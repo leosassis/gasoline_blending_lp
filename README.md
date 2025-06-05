4f654ceacef9601b451317b84d9c46034c681d3c
Leonardo Assis <leosassis@gmail.com>
2025-06-05 12:40:49 -0400

    added a Flask route to display results in a web browser

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ This is a simple project to illustrate initial concepts on:
     - Pyomo
     - Process systems engineering
     - highs solver
+    - Flask
 
 Before running main.py, type in the command line the following: pip install -r requirements.txt 
 
 File linear_blending_model.pdf constains the mathematical model and explanations of the blending problem.
+
+Check results at http://127.0.0.1:5000/
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pyomo.environ as pyo
+from flask import Flask, render_template
 from src.models.build_model import create_model
 
+
+app = Flask(__name__)
 
 def run_optimization() -> None:
     """ 
     Builds, solves, and display the problem.
+    
+    Returns:
+            variable_values: variable results of the optimization problem.
     """
     
     
@@ -15,15 +21,34 @@ def run_optimization() -> None:
     create_model(model)
     
     # Step 3: Solve model
-    pyo.SolverFactory('appsi_highs').solve(model).write()
+    results = pyo.SolverFactory('appsi_highs').solve(model)
+    results.write()
     
     # Step 4: Display variable results
-    model.V_QuantityStreamInProduct.display()    
+    model.V_QuantityStreamInProduct.display()  
+    
+    # Step 5:Extract values from the Pyomo variable
+    variable_values = {index: round(pyo.value(var), 2) for index, var in model.V_QuantityStreamInProduct.items()}
+
+    return variable_values
+
+
+@app.route("/")
+def show_result():
+    """ 
+    Flask route to solve the model and render results in an HTML template.
+    """
+    
+    
+    variable_values = run_optimization()    
+    return render_template("result.html", variable_values=variable_values)
+    
 
 if __name__=="__main__":
     """ 
-    Main function to run the optimization problem.
+    Main entry point to run the Flask app.
     """
     
-    run_optimization()
+    
+    app.run(debug=True)
     
